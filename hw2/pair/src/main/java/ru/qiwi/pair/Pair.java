8bf55ec3a185753cfe74b09b0ed03f11a346f604
d.orlyanskiy <d.orlyanskiy@qiwi.com>
2023-04-10 14:25:36 +0300

    dz 2

--- hw2/pair/src/main/java/ru/qiwi/pair/Pair.java
+++ hw2/pair/src/main/java/ru/qiwi/pair/Pair.java
@@ -1,5 +1,33 @@
 package ru.qiwi.pair;
 
 public class Pair<One, Two> {
+    private final One first;
+    private final Two second;
 
+    private Pair(One first, Two second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public static <T, V> Pair<T, V> of(T first, V second) {
+        return new Pair<>(first, second);
+    }
+
+    public One getFirst() {
+        return first;
+    }
+
+    public Two getSecond() {
+        return second;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj.getClass() != this.getClass()) {
+            return false;
+        }
+
+        Pair<?,?> other = (Pair<?,?>) obj;
+        return this.first == other.first && this.second == other.second;
+    }
 }
